ec163727f000e285e7461804e66a053fe44d7670
little-junior <rampagedolol@outlook.com>
2023-12-19 19:42:08 -0300

    feat: add class Address

---
+++ SalesApp.DomainLayer/Model/Companies/Address.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.DomainLayer.Model.Companies
+{
+    internal class Address
+    {
+
+        public int ID {  get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public string? Complement { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public Address(string zipCode, string street, string? complement, string neighborhood, string city, string state)
+        {
+            ID = GerarID();
+            ZipCode = zipCode;
+            Street = street;
+            Complement = complement;
+            Neighborhood = neighborhood;
+            City = city;
+            State = state;
+        }
+
+        public int GerarID()
+        {
+            return Math.Abs(DateTime.Now.GetHashCode());
+        }
+
+
+        
+    }
+}
