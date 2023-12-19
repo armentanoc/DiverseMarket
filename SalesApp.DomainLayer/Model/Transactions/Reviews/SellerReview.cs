f4d6134051d6332a0564d4a864aa2904e2f3035d
armentanoc <armentanocarolina@gmail.com>
2023-12-19 17:49:37 -0300

    Create Seller Review class inheriting from Review.

--- SalesApp.DomainLayer/Model/Transactions/Reviews/SellerReview.cs
+++ SalesApp.DomainLayer/Model/Transactions/Reviews/SellerReview.cs
@@ -1,6 +1,14 @@
-﻿namespace SalesApp.DomainLayer.Model.Transactions.Reviews
+﻿using SalesApp.DomainLayer.Model.Users;
+
+namespace SalesApp.DomainLayer.Model.Transactions.Reviews
 {
-    internal class SellerReview
+    internal class SellerReview : Review
     {
+
+        public Seller Seller { get; private set; }
+        public SellerReview(ReviewEnum reviewEnum, Seller seller, string? comment = null) : base(reviewEnum, comment)
+        {
+            Seller = seller;
+        }
     }
 }
