3ba936b6dc0e9c1b00ead10bb94bcb62a6ccd509
armentanoc <armentanocarolina@gmail.com>
2023-12-20 07:59:41 -0300

    Adds Selling attribute to ProductReview class.

--- SalesApp.DomainLayer/Model/Transactions/Reviews/ProductReview.cs
+++ SalesApp.DomainLayer/Model/Transactions/Reviews/ProductReview.cs
@@ -4,11 +4,12 @@ namespace SalesApp.DomainLayer.Model.Transactions.Reviews
 {
     internal class ProductReview : Review
     {
-
+        public Selling Selling { get; private set; }
         public ProductGeneral Product { get; private set; }
-        public ProductReview(ReviewEnum reviewEnum, ProductGeneral product, string? comment = null) : base(reviewEnum, comment)
+        public ProductReview(ReviewEnum reviewEnum, ProductGeneral product, Selling selling, string? comment = null) : base(reviewEnum, comment)
         {
             Product = product;
+            Selling = selling;
         }
     }
 }
