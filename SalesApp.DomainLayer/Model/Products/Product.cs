b61580ee1734b510dcf31492691f6abd5348ddbd
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-20 12:16:51 -0300

    fix: adicionando relação entre um produto e diversos vendedores

--- SalesApp.DomainLayer/Model/Products/Product.cs
+++ SalesApp.DomainLayer/Model/Products/Product.cs
@@ -1,18 +1,12 @@
-
-
 namespace SalesApp.DomainLayer.Model.Products
-{
-    using SalesApp.DomainLayer.Model.Users;
 
     internal class Product
     {
         public int Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
-        public decimal Price { get; private set; }
         public decimal AverageRating { get; private set; }
         public decimal NumberOfReviews { get; private set; }
-        public List<Seller> Sellers { get; private set; }
         //implementar imagem?
 
         internal Product(string name, string description, decimal price, Seller firstSeller)
@@ -36,22 +30,6 @@ namespace SalesApp.DomainLayer.Model.Products
             this.AverageRating = review + (this.NumberOfReviews * this.AverageRating);
             this.NumberOfReviews++;
             this.AverageRating /= this.NumberOfReviews;
-        }
-
-        internal bool AddSeller(Seller seller)
-        {
-            if (seller == null || this.Sellers.Contains(seller)) return false;
-
-            this.Sellers.Add(seller);
-
-            return true;
-        }
-
-        internal bool ChangePrice(decimal newPrice)
-        {
-            if(this.Price == 0 || newPrice <= 0) return false;
-
-            this.Price = newPrice; return true;
         }
 
         internal bool ChangeDescription(string newDescription)
