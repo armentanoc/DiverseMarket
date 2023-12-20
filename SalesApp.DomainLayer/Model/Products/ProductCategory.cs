cc9d3f96accda386182aa6d3308490f926d9263b
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-20 10:12:02 -0300

    feat: product category enum

---
+++ SalesApp.DomainLayer/Model/Products/ProductCategory.cs
@@ -0,0 +1,12 @@
+namespace SalesApp.DomainLayer.Model.Products
+{ 
+    public enum ProductCategory
+    {
+        Books = 1,
+        Eletronics = 2,
+        Clothing = 3,
+        Beauty = 4,
+        Jewelry = 5,
+        Groceries = 6
+    }
+}
