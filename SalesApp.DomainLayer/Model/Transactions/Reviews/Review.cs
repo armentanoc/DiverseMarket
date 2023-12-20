dfda96d83bbb91df23d4c8af69ff157f70be957d
armentanoc <armentanocarolina@gmail.com>
2023-12-20 15:41:22 -0300

    Changing Id type from string to int.

--- SalesApp.DomainLayer/Model/Transactions/Reviews/Review.cs
+++ SalesApp.DomainLayer/Model/Transactions/Reviews/Review.cs
@@ -8,13 +8,13 @@ namespace SalesApp.DomainLayer.Model.Transactions.Reviews
 {
     internal abstract class Review
     {
-        public string Id { get; private set; }
+        public int Id { get; private set; }
         public ReviewEnum ReviewEnum { get; private set; }
         public string? Comment { get; private set; }
 
         public Review(ReviewEnum reviewEnum, string? comment = null)
         {
-            Id = Math.Abs(Guid.NewGuid().GetHashCode()).ToString();
+            Id = Math.Abs(Guid.NewGuid().GetHashCode());
             Comment = comment ?? string.Empty;
             ReviewEnum = reviewEnum;
         }
