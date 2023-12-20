3ff34d0bdd8cbb692306fa4cd678e295d9321843
little-junior <rampagedolol@outlook.com>
2023-12-20 17:06:35 -0300

    feat: add Moderator class

--- SalesApp.DomainLayer/Model/Users/Moderator.cs
+++ SalesApp.DomainLayer/Model/Users/Moderator.cs
@@ -1,11 +1,14 @@
 ﻿namespace SalesApp.DomainLayer.Model.Users
 {
-    internal class Moderator
+    public class Moderator : User
     {
-        //        - Autoriza vendedores
+        //- Autoriza vendedores 
         //- Soluciona problemas entre vendedores e compradores
         //- Não pode comprar ou vender dentro do sistema
         //- Entra no sistema com login e senha como todos os usuarios
         //- Cria login e senha temporarios para vendedores
+    
+        public Moderator(string username, string name, string email, string password, int phone) : base(username, name, email, password, phone) { }
+
     }
 }
