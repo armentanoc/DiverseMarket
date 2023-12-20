75054b78e4005a29a6eb3c4ef99574c0888d8419
armentanoc <armentanocarolina@gmail.com>
2023-12-20 07:50:32 -0300

    Revert "Removing unnecessary programs."
    This reverts commit 29e1da05307ee09ccd48de93fe1bd39a0c062413.

---
+++ SalesApp.Infrastructure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesApp.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
