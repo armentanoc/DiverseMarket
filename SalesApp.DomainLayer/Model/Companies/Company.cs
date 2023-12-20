08c44569bb55f1e8c64549936bd8c64e44661a5a
little-junior <rampagedolol@outlook.com>
2023-12-19 21:25:48 -0300

    feat: add Company class

--- SalesApp.DomainLayer/Model/Companies/Company.cs
+++ SalesApp.DomainLayer/Model/Companies/Company.cs
@@ -6,6 +6,29 @@
         //- Cadastradas pelo moderador
         //- Lidas atraves de um json
         //- Podem ser atualizadas pelo moderador
+        public int ID { get; }
+        public string CNPJ { get; private set; }
+        public string CorporateName { get; private set; }
+        public string TradeName { get; private set; }
+        public int? LegalEntityTypeCode { get; private set; }
+        public int? Telephone { get; private set; }
+        public Address Address { get; private set; }
+
+        public Company(string cnpj, string corporateName, string tradeName, Address address, int? legalEntityTypeCode = null, int? telephone = null)
+        {
+            ID = GenerateID();
+            CNPJ = cnpj;
+            CorporateName = corporateName;
+            TradeName = tradeName;
+            LegalEntityTypeCode = legalEntityTypeCode;
+            Telephone = telephone;
+            Address = address;
+        }
+
+        private int GenerateID()
+        {
+            return Math.Abs(DateTime.Now.GetHashCode());
+        }
 
     }
 }
