9b22f04cfe04286010045705dd653eb702556db9
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-20 10:37:46 -0300

    feat: product general e product seller foram combinadas em product

---
+++ SalesApp.DomainLayer/Model/Products/Product.cs
@@ -0,0 +1,62 @@
+
+
+namespace SalesApp.DomainLayer.Model.Products
+{
+    using SalesApp.DomainLayer.Model.Users;
+
+    internal class Product
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal AverageRating { get; private set; }
+        public decimal NumberOfReviews { get; private set; }
+        public List<Seller> Sellers { get; private set; }
+        //implementar imagem?
+
+        internal Product(string name, string description, decimal price, Seller firstSeller)
+        {
+            this.Id = SetId();
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+            Sellers = new List<Seller>();
+            if (firstSeller != null ) { this.Sellers.Add(firstSeller); }
+
+        }
+
+        private int SetId()
+        {
+            return Math.Abs(Guid.NewGuid().GetHashCode()).ToString();
+        }
+
+        internal void AddReview(decimal review)
+        {
+            this.AverageRating = review + (this.NumberOfReviews * this.AverageRating);
+            this.NumberOfReviews++;
+            this.AverageRating /= this.NumberOfReviews;
+        }
+
+        internal bool AddSeller(Seller seller)
+        {
+            if (seller == null || this.Sellers.Contains(seller)) return false;
+
+            this.Sellers.Add(seller);
+
+            return true;
+        }
+
+        internal bool ChangePrice(decimal newPrice)
+        {
+            if(!this.Price.HasValue || newPrice <= 0) return false;
+
+            this.Price = newPrice; return true;
+        }
+
+        internal bool ChangeDescription(string newDescription)
+        {
+            this.Description = this.Description.Replace(newDescription);
+        }
+    }
+}
