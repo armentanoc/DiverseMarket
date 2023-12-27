e54bf564b26ab47cde1b8b68993e359c4e10bedc
armentanoc <armentanocarolina@gmail.com>
2023-12-27 15:28:39 -0300

    Company specifications for database.

---
+++ SalesApp.Infrastructure/Repositories/CompanyDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.Infrastructure.Repositories
+{
+    internal class CompanyDB
+    {
+        internal static string InitializeTable()
+        {
+            return @"
+            CREATE TABLE IF NOT EXISTS Company (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                cnpj VARCHAR(14),
+                corporate_name VARCHAR(45),
+                trade_name VARCHAR(45),
+                User_id INTEGER,
+                FOREIGN KEY (User_id) REFERENCES User(id) ON DELETE NO ACTION ON UPDATE NO ACTION
+            );";
+
+        }
+    }
+}
