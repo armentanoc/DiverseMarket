6a9645a32defcbfbb04e1a78e6a34196051c308c
armentanoc <armentanocarolina@gmail.com>
2023-12-27 10:34:46 -0300

    Fixing Product classname.

--- SalesApp.DomainLayer/Model/Transactions/Reviews/ProductReview.cs
+++ SalesApp.DomainLayer/Model/Transactions/Reviews/ProductReview.cs
@@ -5,8 +5,8 @@ namespace SalesApp.DomainLayer.Model.Transactions.Reviews
     internal class ProductReview : Review
     {
         public Selling Selling { get; private set; }
-        public ProductGeneral Product { get; private set; }
-        public ProductReview(ReviewEnum reviewEnum, ProductGeneral product, Selling selling, string? comment = null) : base(reviewEnum, comment)
+        public Product Product { get; private set; }
+        public ProductReview(ReviewEnum reviewEnum, Product product, Selling selling, string? comment = null) : base(reviewEnum, comment)
         {
             Product = product;
             Selling = selling;
