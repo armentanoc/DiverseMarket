d573927e4fc6d0940deebdf29159da6d43bcbfab
armentanoc <armentanocarolina@gmail.com>
2023-12-27 15:29:25 -0300

    Product and Product Review database specifications.

---
+++ SalesApp.Infrastructure/Repositories/ProductDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.Infrastructure.Repositories
+{
+    internal class ProductDB
+    {
+        internal static string InitializeTable()
+        {
+            return @"
+            CREATE TABLE IF NOT EXISTS Product (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(45) NOT NULL,
+                description VARCHAR(45) NULL
+            );";
+
+        }
+    }
+}
