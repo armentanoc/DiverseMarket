16cde1d39aa9d26bfc199be27af489f2c34abc10
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-27 19:41:38 -0300

    feat: resources folder

---
+++ SalesApp.UI/Resources/Logo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.UI.Resources
+{
+    internal class Logo : PictureBox
+    {
+        internal Logo()
+        {
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Image = Image.FromFile(@"Resources\DiverseMarket.png");
+        }
+    }
+}
