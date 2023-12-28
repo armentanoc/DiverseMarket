8e54d7261af85c9d567b94afc8922c5cab762d3a
armentanoc <armentanocarolina@gmail.com>
2023-12-28 13:26:47 -0300

    Creates a SaveCompanyReview operation in infrastructure.

---
+++ SalesApp.Infrastructure/Operations/SaveCompanyReview.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SalesApp.Infrastructure.Operations
+{
+    public class SaveCompanyReview
+    {
+        public static void Execute(int clientId, int companyId, string review, string comment)
+        {
+            try
+            {
+                using (var connection = DatabaseConnection.Instance.Connection)
+                using (var command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = "INSERT INTO ReviewCompany (Client_id, Company_id, review, comment) VALUES (@ClientId, @CompanyId, @Review, @Comment);";
+
+                    command.Parameters.AddWithValue("@ClientId", clientId);
+                    command.Parameters.AddWithValue("@CompanyId", companyId);
+                    command.Parameters.AddWithValue("@Review", review);
+                    command.Parameters.AddWithValue("@Comment", comment);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving CompanyReview: {ex.Message}");
+            }
+        }
+    }
+}
