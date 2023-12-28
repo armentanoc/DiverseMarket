ab38de0b5d331a8dc2d9a246318ca72661ce766e
armentanoc <armentanocarolina@gmail.com>
2023-12-28 13:25:39 -0300

    Create a DTO for CompanyReview and a Test class with a method to save it in database.

---
+++ SalesApp.DomainLayer/Service/DTOs/CompanyReviewDTO.cs
@@ -0,0 +1,24 @@
+﻿
+using SalesApp.DomainLayer.Model.Transactions.Reviews;
+using SalesApp.Infrastructure.Operations;
+
+namespace SalesApp.DomainLayer.DTOs
+{
+    public class CompanyReviewDTO
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; } 
+        public int CompanyId { get; set; } 
+        public string Review { get; set; }
+        public string? Comment { get; set; }
+        public CompanyReviewDTO(CompanyReview companyReview)
+        {
+            ClientId = companyReview.Customer.Id;
+            CompanyId = companyReview.Company.Id;
+            Review = companyReview.ReviewEnum.ToString();
+            Comment = companyReview.Comment;
+        }
+
+        public void Create() => SaveCompanyReview.Execute(ClientId, CompanyId, Review, Comment);
+    }
+}
---
+++ SalesApp.DomainLayer/Service/Test.cs
@@ -0,0 +1,64 @@
+﻿using SalesApp.DomainLayer.DTOs;
+using SalesApp.DomainLayer.Model.Companies;
+using SalesApp.DomainLayer.Model.Transactions.Reviews;
+using SalesApp.DomainLayer.Model.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.DomainLayer.Service
+{
+    internal class Test
+    {
+        internal static void TrySavingCompanyReview()
+        {
+            var fakeAddress = new Address(
+                 zipCode: "12345-678",
+                 street: "Fake Street",
+                 neighborhood: "Fake Neighborhood",
+                 city: "Fake City",
+                 state: "FS",
+                 complement: "Apt 123");
+
+            var fakeCompany = new Company(
+            username: "fakecompany",
+            name: "Fake Company",
+            email: "fakecompany@example.com",
+            password: "fakepassword",
+            phone: 123456789,
+            address: fakeAddress,
+            cnpj: "12345678901234",
+            corporateName: "Fake Corp",
+            tradeName: "Fake Trade"
+        );
+
+            var fakeCustomer = new Customer(
+           username: "fakecustomer",
+           name: "Fake Customer",
+           email: "fakecustomer@example.com",
+           password: "fakepassword",
+           phone: 987654321,
+           role: RolesEnum.Client,
+           address: fakeAddress
+       );
+
+            string fakeComment = "Excelente! Muito bom";
+
+            // Example CompanyReviewDTO
+            var companyReview = new CompanyReview(
+                ReviewEnum.Excelente,
+                fakeCompany,
+                fakeCustomer,
+                fakeComment
+                );
+
+            // Create an instance of ReviewRepository and save the data
+            var companyReviewDTO = new CompanyReviewDTO(companyReview);
+            companyReviewDTO.Create();
+
+            Console.WriteLine("Dados salvos com sucesso em ReviewCompany!");
+        }
+    }
+}
