31ccfbc2ba4c6498f9f0cabb102e35664db6777b
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-28 12:20:35 -0300

    feat: rounded password box component

---
+++ SalesApp.UI/Components/PasswordRoundedTextBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.UI.Components
+{
+    internal class PasswordRoundedTextBox : RoundedTextBox
+    {
+        public PasswordRoundedTextBox(int width, int height) : base("Senha", width, height)
+        {
+            this.TextBox.Enter += new EventHandler((sender, e) =>
+            {
+                TextBox textBox = sender as TextBox;
+                if (textBox.Text == "Senha")
+                {
+                    textBox.PasswordChar = '*';
+                    textBox.Text = string.Empty;
+                    textBox.UseSystemPasswordChar = true;
+                }
+            });
+            this.TextBox.Leave += new EventHandler((sender, e) =>
+            {
+                TextBox textBox = sender as TextBox;
+                if (string.IsNullOrEmpty(textBox.Text) || textBox.Text.Equals("Senha"))
+                {
+                    textBox.Text = "Senha";
+                    textBox.PasswordChar = '\0';
+                    textBox.UseSystemPasswordChar = false;
+                }
+            });
+        }
+    }
+}
