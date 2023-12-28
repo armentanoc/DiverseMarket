0099c32bd65e0d5f70408a6826dcfa1e165ba0b2
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-28 12:51:17 -0300

    feat: more colors

--- SalesApp.UI/Styles/Colors.cs
+++ SalesApp.UI/Styles/Colors.cs
@@ -9,5 +9,11 @@ namespace SalesApp.UI.Styles
     internal static class Colors
     {
         internal static readonly Color MainBackgroundColor = ColorTranslator.FromHtml("#001E30");
+        internal static readonly Color StrokeTextBox = ColorTranslator.FromHtml("#D1D0D0");
+        internal static readonly Color TextBoxFill = ColorTranslator.FromHtml("#F8FBFF");
+        internal static readonly Color TextBoxForeColor = ColorTranslator.FromHtml("#1F3461");
+        internal static readonly Color CallToActionButton = ColorTranslator.FromHtml("#D3E4F6");
+        internal static readonly Color CallToActionText = ColorTranslator.FromHtml("#0C1D29");
+        internal static readonly Color SecondaryButton = ColorTranslator.FromHtml("#FFFFFF");
     }
 }
