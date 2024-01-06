bf903dd26538755af2236dccb80116a22eab35c4
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-06 18:46:07 -0300

    feat: customer profile page working

---
+++ SalesApp.UI/Pages/Customer/CustomerDataPage.Designer.cs
@@ -0,0 +1,26 @@
+﻿namespace SalesApp.UI.Pages.Customer
+{
+    partial class CustomerDataPage
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent(long userId)
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "CustomerDataPage";
+        }
+
+     
+    }
+}
---
+++ SalesApp.UI/Pages/Customer/CustomerWalletHistoryPage.Designer.cs
@@ -0,0 +1,27 @@
+﻿namespace SalesApp.UI.Pages.Customer
+{
+    partial class CustomerWalletHistoryPage
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent(long userId)
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "CustomerWalletHistoryPage";
+        }
+
+     
+    }
+}
