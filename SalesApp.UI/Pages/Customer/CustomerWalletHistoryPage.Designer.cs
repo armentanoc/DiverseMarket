b9cee7de980658c5ce3b1b62a8ffdbdb2381fc77
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-11 16:36:41 -0300

    feat: customer wallet

--- SalesApp.UI/Pages/Customer/CustomerWalletHistoryPage.Designer.cs
+++ SalesApp.UI/Pages/Customer/CustomerWalletHistoryPage.Designer.cs
@@ -1,10 +1,14 @@
-﻿namespace SalesApp.UI.Pages.Customer
+﻿using SalesApp.UI.Components;
+using SalesApp.UI.Styles;
+
+namespace SalesApp.UI.Pages.Customer
 {
     partial class CustomerWalletHistoryPage
     {
         private System.ComponentModel.IContainer components = null;
+        private long userId;
+        private Button homepageButton;
 
-        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -16,12 +20,71 @@
 
         private void InitializeComponent(long userId)
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CustomerWalletHistoryPage";
+            this.userId = userId;
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1280, 832);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "DiverseMarket";
+            this.BackColor = Colors.MainBackgroundColor;
+            FormClosed += _FormClosed;
+            InitScreen();
         }
 
-     
+        private void InitScreen()
+        {
+            InitLogo();
+            InitLabel();
+            InitButtons();
+        }
+
+        private void InitLabel()
+        {
+            Label pageTitle = new Label();
+            pageTitle.Text = "Reembolsos";
+            pageTitle.Location = new Point(140, 67);
+            pageTitle.AutoSize = true;
+            pageTitle.ForeColor = Color.White;
+            pageTitle.Font = new Font("Ubuntu", 32);
+
+            this.Controls.Add(pageTitle);
+        }
+
+        private void InitLogo()
+        {
+            this.Icon = new Icon(@"Resources\icon.ico");
+
+            Logo logo = new Logo();
+            logo.Location = new Point(1033, 93);
+            logo.Width = 192;
+            logo.Height = 22;
+
+            this.Controls.Add(logo);
+        }
+
+        private void InitButtons()
+        {
+            this.homepageButton = new System.Windows.Forms.Button();
+            this.homepageButton.Location = new Point(37, 67);
+            this.homepageButton.Size = new Size(79, 71);
+            this.homepageButton.FlatStyle = FlatStyle.Flat;
+            this.homepageButton.FlatAppearance.BorderSize = 0;
+            this.homepageButton.Cursor = Cursors.Hand;
+            this.homepageButton.Image = Image.FromFile(@"Resources\logo-reduzida.png");
+            this.homepageButton.BackgroundImageLayout = ImageLayout.Zoom;
+            this.homepageButton.Click += new EventHandler((object sender, EventArgs e) =>
+            {
+                this.Hide();
+                new HomePageCustomer(this.userId).Show();
+            });
+
+            this.Controls.Add(homepageButton);
+        }
+
+        private void _FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+
     }
 }
