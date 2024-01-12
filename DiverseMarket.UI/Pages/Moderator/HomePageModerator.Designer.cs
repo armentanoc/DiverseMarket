a7236d5fd5c01501c0461f8a65d1bab1ff6175f5
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-12 13:54:54 -0300

    feat: init homepagemoderator

--- DiverseMarket.UI/Pages/Moderator/HomePageModerator.Designer.cs
+++ DiverseMarket.UI/Pages/Moderator/HomePageModerator.Designer.cs
@@ -1,4 +1,9 @@
-﻿namespace DiverseMarket.UI.Pages.Moderator
+﻿using DiverseMarket.Backend.Services;
+using DiverseMarket.UI.Components;
+using DiverseMarket.UI.Styles;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DiverseMarket.UI.Pages.Moderator
 {
     partial class HomePageModerator
     {
@@ -7,6 +12,12 @@
         /// </summary>
         private System.ComponentModel.IContainer components = null;
         private long _userId;
+        private RoundedButton companiesList;
+        private RoundedButton categoriesList;
+        private RoundedButton refundList;
+        private RoundedButton logoutButton;
+        private Label helloLabel;
+        private Label textLabel;
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
@@ -29,10 +40,85 @@
         private void InitializeComponent(long userId)
         {
             this._userId = userId;
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "HomePageModerator";
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1280, 832);
+            Name = "LoginPage";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "HomePageModerator";
+            this.BackColor = Colors.MainBackgroundColor;
+            InitScreen(userId);
+        }
+
+
+        private void InitScreen(long userId)
+        {
+            InitComponents(userId);
+        }
+
+        private void InitComponents(long userId)
+        {
+            InitButtons();
+            InitLabels(userId);
+        }
+
+        private void InitLabels(long userId)
+        {
+            string userName = UserService.GetUserFullNameById(userId);
+
+            this.helloLabel = new Label();
+            this.helloLabel.Text = $"Olá, {userName}";
+            this.helloLabel.ForeColor = Colors.SecondaryButton;
+            this.helloLabel.Font = new System.Drawing.Font("Ubuntu", 24);
+            this.helloLabel.Location = new Point(50, 100);
+            this.helloLabel.Size = new Size(500, 100);
+            this.Controls.Add(helloLabel);
+
+            this.textLabel = new Label();
+            this.textLabel.Text = "Selecione uma opção";
+            this.textLabel.ForeColor = Colors.SecondaryButton;
+            this.textLabel.Font = new System.Drawing.Font("Ubuntu", 16);
+            this.textLabel.Location = new Point(50, 250);
+            this.textLabel.Size = new Size(500, 100);
+            this.Controls.Add(textLabel);
+        }
+
+        private void InitButtons()
+        {
+            this.logoutButton = new RoundedButton("Sair", 150, 57, Colors.SecondaryButton, 32);
+            this.logoutButton.Location = new System.Drawing.Point(1080, 50);
+            this.logoutButton.MouseEnter += new EventHandler((object sender, EventArgs e) => { this.logoutButton.Cursor = Cursors.Hand; });
+            //this.logoutButton.Click += new EventHandler(logoutButton_Click);
+            this.Controls.Add(logoutButton);
+
+            this.companiesList = new RoundedButton("Listar Estabelecimentos", 300, 57, Colors.SecondaryButton, 32);
+            this.companiesList.Location = new System.Drawing.Point(50, 300);
+            this.companiesList.MouseEnter += new EventHandler((object sender, EventArgs e) => { this.companiesList.Cursor = Cursors.Hand; });
+            this.companiesList.Click += new EventHandler(companiesList_Click);
+            this.Controls.Add(companiesList);
+
+            this.categoriesList = new RoundedButton("Listar categorias", 300, 57, Colors.SecondaryButton, 32);
+            this.categoriesList.Location = new System.Drawing.Point(370, 300);
+            this.categoriesList.MouseEnter += new EventHandler((object sender, EventArgs e) => { this.categoriesList.Cursor = Cursors.Hand; });
+            this.categoriesList.Click += new EventHandler(categoriesList_Click);
+            this.Controls.Add(categoriesList);
+
+            this.refundList = new RoundedButton("Listar categorias", 300, 57, Colors.SecondaryButton, 32);
+            this.refundList.Location = new System.Drawing.Point(690, 300);
+            this.refundList.MouseEnter += new EventHandler((object sender, EventArgs e) => { this.refundList.Cursor = Cursors.Hand; });
+            //this.refundList.Click += new EventHandler(refundList_Click);
+            this.Controls.Add(refundList);
+        }
+
+        private void companiesList_Click(object sender, EventArgs e)
+        {
+            new CompaniesList().Show();
+            this.Hide();
+        }
+
+        private void categoriesList_Click(object sender, EventArgs e)
+        {
+            new CategoriesList().Show();
+            this.Hide();
         }
 
         #endregion
