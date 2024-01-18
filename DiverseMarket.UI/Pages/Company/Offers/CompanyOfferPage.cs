cbbded3e3815df1b946099be07514cd5d3349afd
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-18 10:49:40 -0300

    refactor: reorganizing offer and removing new product related buttons, since they are going to be in another flow.

---
+++ DiverseMarket.UI/Pages/Company/Offers/CompanyOfferPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiverseMarket.UI.Pages.Company
+{
+    public partial class CompanyOfferPage : Form
+    {
+        public CompanyOfferPage(long userId)
+        {
+            InitializeComponent(userId);
+        }
+    }
+}
---
+++ DiverseMarket.UI/Pages/Company/Offers/CompanySpecificOfferPage.cs
@@ -0,0 +1,12 @@
+﻿using DiverseMarket.Backend.DTOs;
+
+namespace DiverseMarket.UI.Pages.Company
+{
+    public partial class CompanySpecificOfferPage : Form
+    {
+        public CompanySpecificOfferPage(ProductOfferCompleteInfoDTO completeOfferInfo, long userId)
+        {
+            InitializeComponent(completeOfferInfo, userId);
+        }
+    }
+}
