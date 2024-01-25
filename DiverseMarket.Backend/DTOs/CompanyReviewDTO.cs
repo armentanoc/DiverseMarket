933415768382dc2eb89fd2475c646c79a2e6ab77
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-25 16:27:26 -0300

    feat: creating company review DTO.

---
+++ DiverseMarket.Backend/DTOs/CompanyReviewDTO.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DiverseMarket.Backend.DTOs
+{
+    public class CompanyReviewDTO
+    {
+        public long Id { get; set; }
+        public long ClientId { get; set; }
+        public long CompanyId { get; set; }
+        public string Review { get; set; }
+        public string? Comment { get; set; }
+
+        public CompanyReviewDTO(long clientId, long companyId, string review, string comment = "")
+        {
+           ClientId = clientId;
+           CompanyId = companyId;
+           Review = review;
+           Comment = comment;
+        }
+    }
+}
