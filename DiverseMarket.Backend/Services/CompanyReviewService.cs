4a88bd88018e791c537ea7dda37df1f378218bbf
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-25 16:28:03 -0300

    feat: creating a company review service.

---
+++ DiverseMarket.Backend/Services/CompanyReviewService.cs
@@ -0,0 +1,24 @@
+﻿using DiverseMarket.Backend.DTOs;
+using DiverseMarket.Backend.Infrastructure.Repositories;
+
+namespace DiverseMarket.Backend.Services
+{
+    public class CompanyReviewService
+    {
+        public static CompanyReviewDTO GetCompanyReviewDTO(int clientId, int companyId, string review, string? comment)
+        {
+            return new CompanyReviewDTO(clientId, companyId, review, comment);
+        }
+
+        public static void AddCompanyReview(CompanyReviewDTO companyReviewDTO)
+        {
+            ReviewCompanyDB.Insert(
+                companyReviewDTO.ClientId,
+                companyReviewDTO.CompanyId,
+                companyReviewDTO.Review,
+                companyReviewDTO.Comment
+                );
+        }
+
+    }
+}
